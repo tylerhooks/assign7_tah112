b155cbd849318ae6f70371a9518359a455f1ff41
tylerhooks <tah112@txstate.edu>
2016-04-24 14:00:34 -0500

    changes

--- CharStack.cpp
+++ CharStack.cpp
@@ -6,19 +6,32 @@ using namespace std;
 
 // Constructor
  CharStack::CharStack(){
-
-}
-    
+ 	int x, n; 
+ 	stackString.substr(n,x);// returns copy of a substring; n characters long, at postition x 
+  }  
 // Stack operations
-void CharStack::push(char){
-
+void CharStack::push(char str){
+	char n;
+	if (!isFull())
+    stackString.append(str,n);//first n characters of str appended to stackString 
 }
 char CharStack::pop(){
- //return (stackString.size()==0)
+	int x; 
+	if(!isEmpty())
+		stackString.at(x);
+		x--; 
+		return x;
 }
-bool CharStack::isFull() const{
-  // return when memory is full 
+bool CharStack::isFull() const{ 
+    if(stackString.capacity()<stackString.size())//ask if we're allowed to use capacity! 
+		return true; 
+    else 
+    	return false; 
 }
 bool CharStack::isEmpty() const{
 
+	if(stackString.size()==0)
+		return true; 
+	else
+		return false; 
 }
--- Driver.cpp
+++ Driver.cpp
@@ -1,12 +1,23 @@
 # include <iostream>
+# include <string> 
 # include <fstream>
 # include "CharStack.h"
+
 
 using namespace std; 
 
 
 int main(){
 
+	///TEST///
+
+	CharStack s;  
+    
+    //char x ; 
+    //s.push('T'); 
+	//x = s.pop();  
+	//s.pop();  
+	cout << x; 
 
 	
 }
