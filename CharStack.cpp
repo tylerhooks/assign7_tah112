1f63223bb3344127fdeee505e9abfbdc187aefac
tylerhooks <tah112@txstate.edu>
2016-04-25 15:26:26 -0500

    stuff

--- CharStack.cpp
+++ CharStack.cpp
@@ -15,10 +15,11 @@ void CharStack::push(char str){
     stackString.append(str, n);//first n characters of str appended to stackString 
 }
 char CharStack::pop(){
-	int x = stackString.length()-1,y; 
+	int x = stackString.size()-1; 
+	int y;
 	if(!isEmpty())
-		stackString.substr(x,y);
-		return x;
+		stackString.erase(x,y);
+		return x; 
 }
 bool CharStack::isFull() const{ 
    if(stackString.capacity()<stackString.size())//ask if we're allowed to use capacity! 
